418258cd2387f5b6f4501964414abf7853a6a2c2
hamza-hamzii <hmza.ehmad@gmail.com>
2021-05-18 01:00:39 +0500

    test

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button class="ustaad-btn">test</button>
+  </div>
+</template>
+
+<script>
+import { ustaadBtn } from "ustaad-btn";
+
+export default {
+  name: "App",
+  mounted() {
+    console.log(ustaadBtn);
+    setTimeout(() => ustaadBtn(), 3000);
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
